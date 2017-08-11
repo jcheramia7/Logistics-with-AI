94b823d0ec359eb3dd4322450a3b649de308c590
jcheramia7 <johannaheramia@yahoo.com>
2017-08-10 23:53:06 -0700

    added vendor

--- advanced/vendor/2amigos/yii2-google-maps-library/ClientAbstract.php
+++ advanced/vendor/2amigos/yii2-google-maps-library/ClientAbstract.php
@@ -4,6 +4,11 @@
  * @link http://2amigos.us
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
+/**
+ * @copyright Copyright (c) 2014 2amigOS! Consulting Group LLC
+ * @link http://2amigos.us
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
 namespace dosamigos\google\maps;
 
 use Yii;
